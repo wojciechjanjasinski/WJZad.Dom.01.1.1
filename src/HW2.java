97b9cbaf760c0f4e911fa25ff08defe7fa14b916
Wojciech Jasinski <wojtek.jasinski@poczta.fm>
2020-06-01 23:04:19 +0200

    Remove row "6" and add a new one.

--- src/HW2.java
+++ src/HW2.java
@@ -3,8 +3,8 @@ class HW2 {
         System.out.println("Hello World 2");
         System.out.println(true);
         System.out.println(1000);
-        System.out.println(100.5555577777777D);
         System.out.println("a"+"b"+"c"+"d");
         System.out.println("Wprowadzam nowe zmiany w kodzie programu");
+        System.out.println("commit and push");
     }
 }
